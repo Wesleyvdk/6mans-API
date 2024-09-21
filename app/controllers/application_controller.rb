5a395a234423ef385b1384612344a0de3df07b8d
Wesleyvdk <shinxisrl@outlook.com>
2024-09-21 13:25:06 +0200

    added authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate_discord_user
+
+    private
+
+    def authenticate_discord_user
+        auth_header = request.headers[ "Authorization" ]
+        token = auth_header.split( " " ).last if auth_header
+
+        if token  &&  valid_discord_token?(token)
+            # Token is valid, proceed with request
+        else
+            render json: { error: "Unauthorized" }, status: :unauthorized
+        end
+    end
+
+    def valid_discord_token?(token)
+        response = HTTParty.get("https://discord.com/api/v10/users/@me", headers: {'Authorization' => "Bearer #{token}"})
+        response.code == 200
+    end
 end
