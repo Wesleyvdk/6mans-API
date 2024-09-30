6d791371eb7a0b13e0dac757139b47bd8839fbb3
wesleyvdk <shinxisrl@outlook.com>
2024-09-30 13:07:59 +0200

    update scrim migrate

--- db/migrate/20240910151807_create_scrims.rb
+++ db/migrate/20240910151807_create_scrims.rb
@@ -1,8 +1,8 @@
 class CreateScrims < ActiveRecord::Migration[7.2]
   def change
     create_table :scrims do |t|
-      t.references :team_one, null: false, foreign_key: true
-      t.references :team_two, null: false, foreign_key: true
+      t.references :team_one, null: false, foreign_key: { to_table: :teams }
+      t.references :team_two, null: false, foreign_key: { to_table: :teams }
       t.string :status
       t.string :result
 
